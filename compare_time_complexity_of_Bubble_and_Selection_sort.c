ae7021a29b2e259329d7b3ed5252a17bc7c8b121
Shan20122003 <shanr1164@gmail.com>
2026-02-12 11:35:59 +0530

    fixxed

--- compare_time_complexity_of_Bubble_and_Selection_sort.c
+++ compare_time_complexity_of_Bubble_and_Selection_sort.c
@@ -2,33 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 void BubbleSort(int arr[], int len){
-    for(int i=0;i<len-1;i++){
-        for(int j=0;j<len-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+    for(int i = 0; i < len - 1; i++){
+        for(int j = 0; j < len - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-
-
 int main(){
-    date();
+
     int arr[] = {1,43,2,3,97,32,34,2,13,435,76,23,45,67,89,90,12,34,56,78};
     int len = sizeof(arr) / sizeof(arr[0]);
-    float date=  new Date();
-    float start_time = Date();
+
+    time_t start_time = time(NULL);
+
     BubbleSort(arr, len);
-    float end_time = clock();
-    float time_taken = (end_time - start_time) / CLOCKS_PER_SEC;
+
+    time_t end_time = time(NULL);
+
+    double time_taken = difftime(end_time, start_time);
+
     printf("Sorted array: ");
-    for(int i=0;i<len;i++){
-        printf("%d ",arr[i]);
+    for(int i = 0; i < len; i++){
+        printf("%d ", arr[i]);
     }
-    printf("\nTime taken: %f seconds\n", time_taken);
+
+    printf("\nTime taken: %.0f seconds\n", time_taken);
+
+    return 0;
 }
